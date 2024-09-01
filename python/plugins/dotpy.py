06a266445e9ce420c93463684d93a4c7eb5b6d61
CharysseZ <107160622+CharysseZ@users.noreply.github.com>
2024-09-01 11:51:45 +0800

    Update dotpy.py

--- python/plugins/dotpy.py
+++ python/plugins/dotpy.py
@@ -6,73 +6,64 @@ import time
 import re
 import db
 import threading
-from.threads import ThreadPool
-import requests
+from .threads import ThreadPool
 
+class Source (object) :
 
-class Source(object):
-
-    def __init__(self):
+    def __init__ (self):
         self.T = tools.Tools()
         self.now = int(time.time() * 1000)
 
-    def getSource(self):
-        sourceFilePath = '/srv/iptv/python/plugins/dotpy_source'
-        all_lines = []
-        network_addresses = []
-        with open(sourceFilePath, 'r') as f:
+    def getSource (self) :
+        sourcePath = '/srv/iptv/python/plugins/dotpy_source'
+        with open(sourcePath, 'r') as f:
             lines = f.readlines()
-            for line in lines:
-                network_addresses.append(line.strip())
+            total = len(lines)
+            threads = ThreadPool(20)
 
-        threads = ThreadPool(20)
-        for address in network_addresses:
-            response = requests.get(address)
-            if response.status_code == 200:
-                lines = response.text.splitlines()
-                all_lines.extend(lines)
-            else:
-                print(f"Failed to fetch data from {address}. Status code: {response.status_code}")
+            for i in range(0, total):
+                line = lines[i].strip('\n')
+                item = line.split(',', 1)
+                threads.add_task(self.detectData, title = item[0], url = item[1])
+                # thread = threading.Thread(target = self.detectData, args = (item[0], item[1], ), daemon = True)
+                # thread.start()
+                # threads.append(thread)
+            threads.wait_completion()
 
-        total = len(all_lines)
-        for i in range(0, total):
-            line = all_lines[i].strip()
-            item = line.split(',', 1)
-            threads.add_task(self.detectData, title=item[0], url=item[1])
+            # for t in threads:
+            #     t.join()
 
-        threads.wait_completion()
-
-    def detectData(self, title, url):
+    def detectData (self, title, url) :
         print('detectData', title, url)
         info = self.T.fmtTitle(title)
 
         netstat = self.T.chkPlayable(url)
 
-        if netstat > 0:
+        if netstat > 0 :
             cros = 1 if self.T.chkCros(url) else 0
             data = {
-                'title': str(info['id']) if info['id']!= '' else str(info['title']),
-                'url': str(url),
+                'title'  : str(info['id']) if info['id'] != '' else str(info['title']),
+                'url'    : str(url),
                 'quality': str(info['quality']),
-                'delay': netstat,
-                'level': info['level'],
-                'cros': cros,
-                'online': 1,
-                'udTime': self.now,
+                'delay'  : netstat,
+                'level'  : info['level'],
+                'cros'   : cros,
+                'online' : 1,
+                'udTime' : self.now,
             }
             self.addData(data)
             self.T.logger('正在分析[ %s ]: %s' % (str(info['id']) + str(info['title']), url))
-        else:
-            pass  # MAYBE later :P
+        else :
+            pass # MAYBE later :P
 
-    def addData(self, data):
+    def addData (self, data) :
         DB = db.DataBase()
         sql = "SELECT * FROM %s WHERE url = '%s'" % (DB.table, data['url'])
         result = DB.query(sql)
 
-        if len(result) == 0:
+        if len(result) == 0 :
             data['enable'] = 1
             DB.insert(data)
-        else:
+        else :
             id = result[0][0]
             DB.edit(id, data)
